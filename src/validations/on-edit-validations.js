18fdca8ecb1c8132c7d4c966d2b5da333b85f69c
samyounger <sam.younger@gmail.com>
2024-02-25 11:24:49 +0000

    Only activate edit or delete actions on data sheet

--- src/validations/on-edit-validations.js
+++ src/validations/on-edit-validations.js
@@ -1,14 +1,24 @@
+const SHEET_NAME_DATA="data";
+
 export class OnEditValidations {
   constructor(event) {
     this.event = event;
   }
 
-  shouldPopulateCells() {
+  shouldPopulateCells(e) {
+    if (!this.isDataSheet()) {
+      return false;
+    }
+
     const column = this.column();
     return !column.isEmpty && column.isSecondColumn;
   }
 
   shouldDeleteCells() {
+    if (!this.isDataSheet()) {
+      return false;
+    }
+
     const column = this.column();
     return column.isEmpty && column.isSecondColumn;
   }
@@ -18,5 +28,9 @@ export class OnEditValidations {
       isEmpty: this.event.range.getValue() === '',
       isSecondColumn: this.event.range.getColumn() === 2,
     };
+  }
+
+  isDataSheet() {
+    return this.event.range.getSheet().getName() === SHEET_NAME_DATA
   }
 }
--- src/validations/on-edit-validations.test.js
+++ src/validations/on-edit-validations.test.js
@@ -4,62 +4,93 @@ const createEvent = (options = {}) => ({
   range: {
     getValue: () => options.value,
     getColumn: () => options.column,
+    getSheet: () => ({
+      getName: () => options.sheetName,
+    }),
   },
 });
 
 
 describe('OnEditValidations', () => {
   describe('#shouldPopulateCells', () => {
-    describe('when the cell selected is empty', () => {
-      const subject = new OnEditValidations(createEvent({ column: 2, value: '' }))
+    describe('when the sheet is no the required one', () => {
+      const sheetName = 'wrong-name';
+
+      const subject = new OnEditValidations(createEvent({ column: 2, value: '', sheetName }))
 
       it('should return false', () => {
         expect(subject.shouldPopulateCells()).toEqual(false);
       });
     });
 
-    describe('when the cell selected is not empty', () => {
-      describe('when the column is not the second row', () => {
-        const subject = new OnEditValidations(createEvent({ column: 3, value: 'B123:B123' }))
+    describe('when the sheet is the required one', () => {
+      const sheetName = 'data';
+
+      describe('when the cell selected is empty', () => {
+        const subject = new OnEditValidations(createEvent({ column: 2, value: '', sheetName }))
 
         it('should return false', () => {
           expect(subject.shouldPopulateCells()).toEqual(false);
         });
       });
 
-      describe('when the column is the second column', () => {
-        const subject = new OnEditValidations(createEvent({ column: 2, value: 'B123:B123' }))
+      describe('when the cell selected is not empty', () => {
+        describe('when the column is not the second row', () => {
+          const subject = new OnEditValidations(createEvent({ column: 3, value: 'B123:B123', sheetName }))
 
-        it('should return true', () => {
-          expect(subject.shouldPopulateCells()).toEqual(true);
+          it('should return false', () => {
+            expect(subject.shouldPopulateCells()).toEqual(false);
+          });
+        });
+
+        describe('when the column is the second column', () => {
+          const subject = new OnEditValidations(createEvent({ column: 2, value: 'B123:B123', sheetName }))
+
+          it('should return true', () => {
+            expect(subject.shouldPopulateCells()).toEqual(true);
+          });
         });
       });
     });
   });
 
   describe('#shouldDeleteCells', () => {
-    describe('when the cell selected is empty', () => {
-      const subject = new OnEditValidations(createEvent({ column: 2, value: '' }))
+    describe('when the sheet is no the required one', () => {
+      const sheetName = 'wrong-name';
 
-      it('should return true', () => {
-        expect(subject.shouldDeleteCells()).toEqual(true);
+      const subject = new OnEditValidations(createEvent({ column: 2, value: '', sheetName }))
+
+      it('should return false', () => {
+        expect(subject.shouldPopulateCells()).toEqual(false);
       });
     });
 
-    describe('when the cell selected is not empty', () => {
-      describe('when the column is not the second row', () => {
-        const subject = new OnEditValidations(createEvent({ column: 3, value: 'B123:B123' }))
+    describe('when the sheet is the required one', () => {
+      const sheetName = 'data';
 
-        it('should return false', () => {
-          expect(subject.shouldDeleteCells()).toEqual(false);
+      describe('when the cell selected is empty', () => {
+        const subject = new OnEditValidations(createEvent({ column: 2, value: '', sheetName }))
+
+        it('should return true', () => {
+          expect(subject.shouldDeleteCells()).toEqual(true);
         });
       });
 
-      describe('when the column is the second column', () => {
-        const subject = new OnEditValidations(createEvent({ column: 2, value: 'B123:B123' }))
+      describe('when the cell selected is not empty', () => {
+        describe('when the column is not the second row', () => {
+          const subject = new OnEditValidations(createEvent({ column: 3, value: 'B123:B123', sheetName }))
 
-        it('should return true', () => {
-          expect(subject.shouldDeleteCells()).toEqual(false);
+          it('should return false', () => {
+            expect(subject.shouldDeleteCells()).toEqual(false);
+          });
+        });
+
+        describe('when the column is the second column', () => {
+          const subject = new OnEditValidations(createEvent({ column: 2, value: 'B123:B123', sheetName }))
+
+          it('should return true', () => {
+            expect(subject.shouldDeleteCells()).toEqual(false);
+          });
         });
       });
     });
